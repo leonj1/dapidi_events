35fecd6ad1f7c83af93d32cfa33a0a760c2fb4e3
Jose Leon <leonj1@gmail.com>
2017-05-19 20:03:10 -0400

    Adding classes to read from Properties file

--- src/main/java/com/dapidi/events/App.java
+++ src/main/java/com/dapidi/events/App.java
@@ -1,13 +1,34 @@
 package com.dapidi.events;
 
+
+import com.dapidi.events.configs.AppProperties;
+import com.dapidi.events.configs.FindResourceFile;
+import com.dapidi.events.configs.GetProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+    private static final String APPLICATION_PROPERTIES = "application.properties";
+
+    public static void main( String[] args ) {
+        log.info("Some succeed because they are destined to. Most succeed because they are determined to. -- Unknown");
+
+        // Starting here, we are going the job Spring would normally do
+        AppProperties appProperties = null;
+        try {
+            appProperties = new AppProperties(
+                    new GetProperty(
+                            new FindResourceFile(APPLICATION_PROPERTIES)
+                    )
+            );
+        } catch (Exception e) {
+            log.error(String.format("Really bad problem trying to find resource %s", APPLICATION_PROPERTIES));
+            System.exit(1);
+        }
     }
 }
